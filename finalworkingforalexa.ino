af24c666f7b0b25cd38c41e868c26f98c99fb088
default-cybe <kaivalyaahir@pnahir.com>
2025-09-18 20:27:50 -0400

    Add turnOn handler for the eight devices

--- finalworkingforalexa.ino
+++ finalworkingforalexa.ino
@@ -34,3 +34,58 @@ bool isConnected = false;
 
 // deviceId is the ID assgined to your smart-home-device in sinric.com dashboard. Copy it from dashboard and paste it here
 
+void turnOn(String deviceId) {
+  if (deviceId == "5d9358b0de917d27fed3d2a1") // Device ID of first device
+  {  
+    Serial.print("Turn on device id: ");
+    Serial.println(deviceId);
+    digitalWrite(device_1, LOW);
+  } 
+  else if (deviceId == "5d9358e4de917d27fed3d2ae") // Device ID of second device
+  { 
+    Serial.print("Turn on device id: ");
+    Serial.println(deviceId);
+    digitalWrite(device_2, LOW);
+  }
+  else if (deviceId == "5d9358ebde917d27fed3d2b4") // Device ID of Third device
+  { 
+    Serial.print("Turn on device id: ");
+    Serial.println(deviceId);
+    digitalWrite(device_3, LOW);
+  }
+  else if (deviceId == "5d9358f6de917d27fed3d2b9") // Device ID of Fourth device
+  { 
+    Serial.print("Turn on device id: ");
+    Serial.println(deviceId);
+    digitalWrite(device_4, LOW);
+  }
+  else if (deviceId == "5d935900de917d27fed3d2be") // Device ID of Fifth device
+  { 
+    Serial.print("Turn on device id: ");
+    Serial.println(deviceId);
+    digitalWrite(device_5, LOW);
+  }
+  else if (deviceId == "5d935907de917d27fed3d2c0") // Device ID of Sixth device
+  { 
+    Serial.print("Turn on device id: ");
+    Serial.println(deviceId);
+    digitalWrite(device_6, LOW);
+  }
+  else if (deviceId == "5d93590fde917d27fed3d2c5") // Device ID of Seventh device
+  { 
+    Serial.print("Turn on device id: ");
+    Serial.println(deviceId);
+    digitalWrite(device_7, LOW);
+  }
+  else if (deviceId == "5d935916de917d27fed3d2ca") // Device ID of Eighth device
+  { 
+    Serial.print("Turn on device id: ");
+    Serial.println(deviceId);
+    digitalWrite(device_8, LOW);
+  }
+  else {
+    Serial.print("Turn on for unknown device id: ");
+    Serial.println(deviceId);    
+  }     
+}
+
